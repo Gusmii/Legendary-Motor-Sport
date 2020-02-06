c4388f5e0f113a875950bca9f0830ec16b706103
Gusmii <agusmanop@gmail.com>
2020-02-06 12:06:55 +0100

    Por fin me decido

---
+++ View/js/index.js
@@ -0,0 +1,11 @@
+$( document ).ready(function(){
+
+  $.ajax({
+    url: "coches.json", success: function (imagenes) {     //Para cargar las imagenes y guardar maximos
+
+      console.log(result);
+       
+    }
+});
+
+});
